9670e400c0692b2edaa6c985f8d1505d644ae957
Tux <tux@netz39.de>
2016-08-13 00:02:40 +0200

    Helper lib for mosquitto

---
+++ mosqhelper.h
@@ -0,0 +1,21 @@
+/*
+ * Helper Lib for libmosquitto
+ */
+
+#pragma once
+
+#include <mosquitto.h>
+
+
+int mqtt_init(const char* clientname,
+	      struct mosquitto **mosq,
+	      void **mqtt_obj);
+
+int mqtt_connect(struct mosquitto *mosq,
+                 const char* host,
+		 const int port,
+		 const int retries);
+
+int mqtt_loop(struct mosquitto *mosq);
+
+int mqtt_close(struct mosquitto *mosq);
