dbb48e4e9e79ddb17dc5a7fa2139b299a90822ed
Alexander Dahl <alex@netz39.de>
2017-12-04 13:19:35 +0100

    test: Introduce unit testing with cmocka
    For now only a very basic test is included and integrated into CMake
    build.
    
    Travis config is updated with this right away, because builds might fail
    without listing the needed package.

---
+++ test/mqtta-test-null.c
@@ -0,0 +1,23 @@
+/*
+ * Copyright 2008 Google Inc.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ * License-Filename: LICENSES/Apache-2.0.txt
+ */
+
+#include <stdarg.h>
+#include <stddef.h>
+#include <setjmp.h>
+#include <cmocka.h>
+
+/* A test case that does nothing and succeeds. */
+static void null_test_success(void **state) {
+    (void) state; /* unused */
+}
+
+int main(void) {
+    const struct CMUnitTest tests[] = {
+        cmocka_unit_test(null_test_success),
+    };
+    return cmocka_run_group_tests(tests, NULL, NULL);
+}
